762e303ce310e78058fce041f12371e1fccc0496
tcbetka <tcbetka@hotmail.com>
2015-08-02 16:14:49 -0500

    Update BMA180Accelerometer.h

--- BMA180Accelerometer.h
+++ BMA180Accelerometer.h
@@ -12,50 +12,50 @@
 
 enum BMA180_RANGE
 {
-	PLUSMINUS_1_G 		= 0,
-	PLUSMINUS_1POINT5_G = 1,
-	PLUSMINUS_2G 		= 2,
-	PLUSMINUS_3G 		= 3,
-	PLUSMINUS_4G 		= 4,
-	PLUSMINUS_8G 		= 5,
-	PLUSMINUS_16G 		= 6
+    PLUSMINUS_1_G 		= 0,
+    PLUSMINUS_1POINT5_G 	= 1,
+    PLUSMINUS_2G 		= 2,
+    PLUSMINUS_3G 		= 3,
+    PLUSMINUS_4G 		= 4,
+    PLUSMINUS_8G 		= 5,
+    PLUSMINUS_16G 		= 6
 };
 
 enum BMA180_BANDWIDTH
 {
-	BW_10HZ 	= 0,
-	BW_20HZ 	= 1,
-	BW_40HZ 	= 2,
-	BW_75HZ 	= 3,
-	BW_150HZ 	= 4,
-	BW_300HZ 	= 5,
-	BW_600HZ 	= 6,
-	BW_12OOHZ 	= 7,
-	BW_HIGHPASS = 8,
-	BW_BANDPASS = 9
+    BW_10HZ 	= 0,
+    BW_20HZ 	= 1,
+    BW_40HZ 	= 2,
+    BW_75HZ 	= 3,
+    BW_150HZ 	= 4,
+    BW_300HZ 	= 5,
+    BW_600HZ 	= 6,
+    BW_12OOHZ 	= 7,
+    BW_HIGHPASS = 8,
+    BW_BANDPASS = 9
 };
 
 enum BMA180_MODECONFIG
 {
-	MODE_LOW_NOISE = 0,
-	MODE_LOW_POWER = 3
+    MODE_LOW_NOISE = 0,
+    MODE_LOW_POWER = 3
 };
 
 class BMA180Accelerometer
 {
 public:
-	BMA180Accelerometer(int bus, int address);
-	virtual ~BMA180Accelerometer();
+    BMA180Accelerometer(int bus, int address);
+    virtual ~BMA180Accelerometer();
 
-	void displayMode(int iterations);
-	int  readFullSensorState();
+    void displayMode(int iterations);
+    int  readFullSensorState();
 
-	// The following do physical reads and writes of the sensors
-	int setRange(BMA180_RANGE range);
+    // The following do physical reads and writes of the sensors
+    int setRange(BMA180_RANGE range);
     BMA180_RANGE getRange();
-	int setBandwidth(BMA180_BANDWIDTH bandwidth);
+    int setBandwidth(BMA180_BANDWIDTH bandwidth);
     BMA180_BANDWIDTH getBandwidth();
-	int setModeConfig(BMA180_MODECONFIG mode);
+    int setModeConfig(BMA180_MODECONFIG mode);
     BMA180_MODECONFIG getModeConfig();
 
 	// Data accessors
@@ -67,26 +67,25 @@ public:
     float getTemperature();
 
 private:
-	int I2CBus, I2CAddress;
-	char dataBuffer[BMA180_I2C_BUFFER];
+    int I2CBus, I2CAddress;
+    char dataBuffer[BMA180_I2C_BUFFER];
 
-	int accelerationX;
-	int accelerationY;
-	int accelerationZ;
-	double pitch;  //in degrees
-	double roll;   //in degrees
-	float temperature; //accurate to 0.5C
+    int accelerationX;
+    int accelerationY;
+    int accelerationZ;
+    double pitch;  //in degrees
+    double roll;   //in degrees
+    float temperature; //accurate to 0.5C
 
-	BMA180_RANGE range;
-	BMA180_BANDWIDTH bandwidth;
-	BMA180_MODECONFIG modeConfig;
+    BMA180_RANGE range;
+    BMA180_BANDWIDTH bandwidth;
+    BMA180_MODECONFIG modeConfig;
 
+    int  convertAcceleration(int msb_addr, int lsb_addr);
+    int  writeI2CDeviceByte(char address, char value);
+    void calculatePitchAndRoll();
 
-	int  convertAcceleration(int msb_addr, int lsb_addr);
-	int  writeI2CDeviceByte(char address, char value);
-	void calculatePitchAndRoll();
-
-	//char readI2CDeviceByte(char address);
+    //char readI2CDeviceByte(char address);
 };
 
 #endif /* BMA180ACCELEROMETER_H_ */
